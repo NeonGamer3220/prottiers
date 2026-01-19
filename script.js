aeeeeec4e1b188351abaded9f84f1a9fd712df75
NeonGamer3220 <neon.gamer322@gmail.com>
2026-01-19 18:51:37 +0100

    Remove aria-selected attribute from tabs
    Remove setting aria-selected attribute for tabs.

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ function showTab(key){
   tabs.forEach(t => {
     const on = t.dataset.tab === key;
     t.classList.toggle("active", on);
-    t.setAttribute("aria-selected", on ? "true" : "false");
   });
 
   panes.forEach(p => p.classList.toggle("show", p.id === `tab-${key}`));
